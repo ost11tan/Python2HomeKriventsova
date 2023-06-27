9aff18f52d6cd89fcb701316ba34bbedd38e3c13
tati <ost11tan@gmail.com>
2023-06-27 05:18:47 +0300

    task2

---
+++ Less3/main.py
@@ -0,0 +1,17 @@
+import task17
+import task18
+import task2
+import task3
+import task4
+
+
+
+task = int(input("Введите номер задания : "))
+if task==1:
+    task1.main()
+elif task==2:
+    task2.main()
+elif task==3:
+    task3.function()
+else:
+    print("Такого задания нет")
