b1463f3448f7fddacbae1a07e0998ac2f443c516
tati <ost11tan@gmail.com>
2023-06-28 10:22:50 +0300

    task3

--- Less4/main.py
+++ Less4/main.py
@@ -2,17 +2,24 @@ import task1
 import task2
 import task3
 import task4
+"""ВОПРОС!!!
+Ранее мы писали в главном файле,при вызове функции из другого файла task1.main
+теперь,при выборе task1. автоматически main дает выбрать только в таком виде
+Я понимаю,что данное оформление не верно,но не могу сообразить как должно выглядеть
+Если в файлах пишу if __name__=='__task1__' ,то не запускается сам файл"""
 
 
 if __name__=='__main__':
     task = int(input("Введите номер задания : "))
     if task==1:
-        task2.main()
+        if __name__ == '__main__':
+            task1
     elif task==2:
         if __name__ == '__main__':
             task2
     elif task==3:
-        task3.main()
+        if __name__ == '__main__':
+            task3
     elif task==4:
         if __name__ == '__main__':
             task4
--- Less4/task3.py
+++ Less4/task3.py
@@ -4,17 +4,16 @@
 ключ не хешируем, используйте его строковое представление."""
 
 
-def function(key:int|str)->dict:
-
-
-
+def function(**kwargs)->dict:
+    res_dict = {}
+    for key, value in kwargs.items():
+        if value.__hash__ == None:
+            value = str(value)
+        res_dict[value] = key
+    return res_dict
 
 
 if __name__=='__main__':
-    print(function())
-
-
-
-
+    print(function(first=1,second="слово",third=(1,2,3,4,6,8),fourth={1,2,3,4,6,8},fifth=[10,4,2,3,6,8],sixth=""))
 
 
