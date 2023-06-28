2474b285a7d1d4bf2606812b153987876c9dfe19
tati <ost11tan@gmail.com>
2023-06-28 11:18:35 +0300

    task1

--- Less4/task1.py
+++ Less4/task1.py
@@ -5,4 +5,19 @@
 ✔ Значения не удаляются, а помещаются в одноимённые переменные без s на конце."""
 
 
+def function(data:list[str])->list[str]:
+    temp = []
+    for i in range(len(data)):
+        if data[i][-1:]=="s" and len(data[i])!=1:
+            temp.append(data[i])
+            data[i] = None
 
+    for i in range (len(temp)):
+        data.append(temp[i])
+
+    return data
+
+
+if __name__=='__main__':
+    new_data=["s", "task", "song", "parts", "friends", "374586940","qwertys","123"]
+    print(function(new_data))
