90b525b909126d1bf89cc57514b5f77d0d6c771a
tati <ost11tan@gmail.com>
2023-07-04 08:10:22 +0300

    task2

---
+++ Less5/task2.py
@@ -0,0 +1,19 @@
+"""Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
+ Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла."""
+
+
+def way(str_imput):
+    path=str_imput.split("\\")
+    name=str_imput.split("\\")[-1]
+    path.pop()
+    type=name.split(".")[-1]
+    name=name.split(".")[0]
+    res=(path,name,type)
+    return res
+
+
+
+
+if __name__=="__main__":
+    data = input("Введите любую строку  : ")
+    print(way(data))
