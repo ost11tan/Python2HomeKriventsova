87e64a4a46bbd8d9a17e21df6cf8c4a47fdbaf87
tati <ost11tan@gmail.com>
2023-07-04 10:27:06 +0300

    фибоноччи

--- Less5/task4.py
+++ Less5/task4.py
@@ -1,1 +1,15 @@
 """Создайте функцию генератор чисел Фибоначчи (см. Википедию)."""
+def febb(amount:int):
+    a, b = 0, 1
+    for __ in range(amount):
+        yield a
+        a, b = b, a + b
+
+
+if __name__ =="__main__":
+    LEN = int(input("Введите количество элементов : "))
+    if LEN>0:
+        for item in febb(LEN):
+            print(item, end=" ")
+    else:
+        print(("Введите количество элементов >0 "))
