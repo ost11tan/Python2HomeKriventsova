69a1d3bdbb8ed6e15846f0da31903096b8b18022
tati <ost11tan@gmail.com>
2023-07-08 01:04:53 +0300

    шахматы без рандома

---
+++ Less6/main.py
@@ -0,0 +1,23 @@
+import task1
+import task2
+import task3
+import task4
+
+
+
+if __name__=='__main__':
+    task = int(input("Введите номер задания : "))
+    if task==1:
+        if __name__ == '__main__':
+            task1
+    elif task==2:
+        if __name__ == '__main__':
+            task2
+    elif task==3:
+        if __name__ == '__main__':
+            task3
+    elif task==4:
+        if __name__ == '__main__':
+            task4
+    else:
+        print("Такого задания нет")
---
+++ Less6/task3.py
@@ -0,0 +1,67 @@
+"""Добавьте в пакет, созданный на семинаре шахматный модуль.
+Внутри него напишите код, решающий задачу о 8 ферзях.
+Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга.
+Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
+Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей.
+Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
+"""
+
+def filling_array(n:int)->list:
+    list_of_CHES = []
+    for i in range(n):
+        s=input(f"{i+1}:")
+        list_of_CHES.append(str_to_tuple(s))
+    return list_of_CHES
+
+def str_to_tuple(s:str)->tuple():
+    new=s.split(",")
+    for item in range(len(new)):
+        new[item]=check(new[item])
+    return  int(new[0]),int(new[1])
+
+def check(s:str)->str:
+    global COLOMN
+    while True:
+        tmp=s
+        if not tmp.isnumeric() or int(tmp)<1 or int(tmp)>COLOMN:
+            s = input(f"{tmp} не число в диапазоне от 1 до {COLOMN}, введите число вместо {tmp} ")
+        else:
+            break
+    return tmp
+
+def chess(arr:list):
+    global COLOMN
+    list_x=[*[item[0] for item in arr]]
+    list_y = [*[item[1] for item in arr]]
+    if ort(list_x)==1 and ort(list_y)==1 and diagonal(list_x, list_y)==1:
+                print("не бьет")
+    else:
+        print("бьет")
+
+
+
+
+def ort(arr:list)->bool:
+    flag=1
+    for i in range(COLOMN):
+        temp=arr[i]
+        for j in range(i + 1, COLOMN):
+            if arr[j]==temp:
+                flag=0
+                return flag
+    return flag
+def diagonal(arr_x:list,arr_y:list)->bool:
+    flag=1
+    for i in range(COLOMN):
+        for j in range(i + 1, COLOMN):
+            if abs(arr_x[i] - arr_x[j]) == abs(arr_y[i] - arr_y[j]):
+                flag=0
+                return flag
+    return flag
+
+
+
+if __name__ =="__main__":
+    LEN = COLOMN=8
+    print("Введите координату по горизонтали и координату по вертикали через запятую")
+    chess(filling_array(LEN))
